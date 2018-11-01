95f5b30426aad2eb3a8e12ed5fcdb22627f9a3f4
郑铭 <zhengming809@pingan.com.cn>
2018-11-01 23:52:46 +0800

    modify shell.js

--- shell.js
+++ shell.js
@@ -1,6 +1,20 @@
 require('shelljs/global');
 const readline = require('readline');
 
+// 输入内容
+const inputMsg = (cnt) => {
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(`${cnt}`, (msg) => {
+            rl.close();
+            resolve(msg);
+        });
+    });
+}
+
 // 去到哪个目录
 const cdDir = () => {
     return new Promise((resolve, reject) => {
@@ -23,38 +37,66 @@ const askBranch = () => {
         let branchExec = exec('git symbolic-ref --short -q HEAD');
         let branch = branchExec.stdout;
         branch = branch.replace('\n', '');
-        rl.question(`当前在这个分支吗: ${branch} (Y/N) `, (msg) => {
-            if (msg && msg.toLowerCase() === 'y') {
-                resolve(branch);
-            } else {
-                reject('askBranch 暂停了');
-            }
-        });
+        inputMsg(`当前在这个分支吗: ${branch} (Y/N) `)
+            .then(msg => {
+                if (msg && msg.toLowerCase() === 'y') {
+                    resolve(branch);
+                } else {
+                    reject('askBranch 暂停了');
+                }
+            });
     });
 }
 
 const pullBranch = (branch) => {
     return new Promise((resolve, reject) => {
+        console.log('正在拉取分支...');
         let code = exec(`git pull origin ${branch}`).code;
         if (code === 0) {
             console.log('git pull success');
-            resolve();
+            resolve(branch);
         } else {
             reject(code);
         }
     });
 }
 
+const pushCode = (branch) => {
+    return new Promise((resolve, reject) => {
+        return inputMsg('\n请输入 commit 内容: ')
+            .then(msg => {
+                if (msg.length < 2) {
+                    reject('commit 内容太短');
+                    return;
+                }
+                if (exec('git add .').code !== 0) {
+                    reject('Error: Git add failed');
+                    return;
+                }
+                if (exec(`git commit -m "${msg}"`).code !== 0) {
+                    reject('Error: Git commit failed');
+                    return;
+                }
+                if (exec('git push').code !== 0) {
+                    reject('Error: Git push failed');
+                    return;
+                }
+            });
+    });
+
+}
+
 Promise.resolve()
     .then(() => cdDir())
     .then(() => askBranch())
     .then(branch => pullBranch(branch))
+    .then(branch => pushCode(branch))
     .then(() => {
         console.log(`congratulations!!!!`);
         exit(1);
     })
     .catch(err => {
-        console.log(`操作失败: ${err}`);
+        console.log(`\n操作失败: ${err}`);
         exit(1);
     });
 
@@ -73,24 +115,7 @@ Promise.resolve()
 //     // console.log(q.stdout);
 
 
-//     if (exec('git add .').code !== 0) {
-//         echo('Error: Git add failed');
-//         exit(1);
-//     }
 
-//     console.log(2);
-
-//     if (exec(`git commit -m "${msg}"`).code !== 0) {
-//         echo('Error: Git commit failed');
-//         exit(1);
-//     }
-
-//     console.log(3);
-
-//     if (exec('git push').code !== 0) {
-//         echo('Error: Git push failed');
-//         exit(1);
-//     }
 
 //     console.log(4);
 // });
