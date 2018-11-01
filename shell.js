476609b429ea3a476c108ac0fba3778e8b6ac3c8
郑铭 <zhengming809@pingan.com.cn>
2018-11-01 22:07:45 +0800

    Auto-commit

--- shell.js
+++ shell.js
@@ -7,23 +7,38 @@ console.log(1);
 //     exit(1);
 // }
 
-if (exec('git add .').code !== 0) {
-    echo('Error: Git add failed');
-    exit(1);
-}
+const readline = require('readline');
 
-console.log(2);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-if (exec('git commit -m "Auto-commit"').code !== 0) {
-    echo('Error: Git commit failed');
-    exit(1);
-}
+rl.question('你认为 Node.js 中文网怎么样？', (answer) => {
+  // 对答案进行处理
+  console.log(`多谢你的反馈：${answer}`);
+  rl.close();
 
-console.log(3);
+    if (exec('git add .').code !== 0) {
+        echo('Error: Git add failed');
+        exit(1);
+    }
 
-if (exec('git push').code !== 0) {
-    echo('Error: Git push failed');
-    exit(1);
-}
+    console.log(2);
 
-console.log(4);
+    if (exec('git commit -m "Auto-commit"').code !== 0) {
+        echo('Error: Git commit failed');
+        exit(1);
+    }
+
+    console.log(3);
+
+    if (exec('git push').code !== 0) {
+        echo('Error: Git push failed');
+        exit(1);
+    }
+
+    console.log(4);
+});
+
+
