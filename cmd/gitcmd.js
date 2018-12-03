3c367aa04b9a65a3520ecfe74cb2ff93d2773c02
郑铭 <zhengming809@pingan.com.cn>
2018-12-03 15:43:48 +0800

    update gitcmd

--- cmd/gitcmd.js
+++ cmd/gitcmd.js
@@ -1,6 +1,11 @@
 const git = require('./git');
+const log = require('./com/log');
 
 git('./')
+    .then(() => {
+        log.success('git push success!!!');
+    })
+    .then(() => git('../biyibi_new_skin'))
     .then(() => {
         log.success('git push success!!!');
     });
