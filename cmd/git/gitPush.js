9bfdf48decf39c1000f59b3819f9ff28126d5ce0
郑铭 <zhengming809@pingan.com.cn>
2019-03-18 11:06:36 +0800

    移除所有项目

--- cmd/git/gitPush.js
+++
@@ -1,9 +0,0 @@
-const submit = require('./submit');
-const log = require('../com/log');
-
-module.exports = function () {
-    return Promise.resolve()
-        .then(() => log.org('正在 git push........', 1))
-        .then(() => submit.push())
-        .then(() => log.tip('git push success', 1));
-}
